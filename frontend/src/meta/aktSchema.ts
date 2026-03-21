20ebd2cecdd764a7ceed020151cbf8869b4b7dba
Marc Merten <marc.merten.1234@gmail.com>
2026-03-21 11:44:42 +0100

    - Changed some schemas

--- frontend/src/meta/aktSchema.ts
+++ frontend/src/meta/aktSchema.ts
@@ -6,5 +6,6 @@ export const aktSchema: MetaTypeSchema = {
     { key: 'title', label: 'Titel', type: 'input', placeholder: 'Titel...', defaultValue: '' },
     { key: 'description', label: 'Beschreibung', type: 'textarea', placeholder: 'Beschreibung...', defaultValue: '' },
     { key: 'location', label: 'Lokation', type: 'input', placeholder: 'Lokation...', defaultValue: '' },
+    { key: 'time', label: 'Zeit', type: 'input', placeholder: 'Zeit...', defaultValue: '' },
   ],
 };
--- frontend/src/meta/szeneSchema.ts
+++ frontend/src/meta/szeneSchema.ts
@@ -5,5 +5,14 @@ export const szeneSchema: MetaTypeSchema = {
   fields: [
     { key: 'title', label: 'Titel', type: 'input', placeholder: 'Titel...', defaultValue: '' },
     { key: 'description', label: 'Beschreibung', type: 'textarea', placeholder: 'Beschreibung...', defaultValue: '' },
+    { key: 'location', label: 'Lokation', type: 'input', placeholder: 'Lokation...', defaultValue: '' },
+    { key: 'time', label: 'Zeit', type: 'input', placeholder: 'Zeit...', defaultValue: '' },
+
+    { key: 'characters', label: 'Charaktere', type: 'textarea', placeholder: 'Charakter hinzufügen...', defaultValue: '' },
+    { key: 'goal', label: 'Ziel der Szene', type: 'input', placeholder: 'Was will der Protagonist erreichen?', defaultValue: '' },
+    { key: 'conflict', label: 'Konflikt', type: 'textarea', placeholder: 'Was steht im Weg?', defaultValue: '' },
+    { key: 'outcome', label: 'Ergebnis', type: 'textarea', placeholder: 'Wie endet die Szene?', defaultValue: '' },
+    { key: 'tone', label: 'Stimmung', type: 'input', placeholder: '', defaultValue: '' },
+    { key: 'importance', label: 'Bedeutung', type: 'input', defaultValue: 'setup' },
   ],
 };
