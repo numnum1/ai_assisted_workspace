6295ab046aa39893b329cf9ce59ffe5f3074a675
Marc Merten <merten@eecc.info>
2026-05-11 13:18:37 +0200

    - Added project old vs neu scrolling

--- frontend/src/features/chatting/project/Project.tsx
+++ frontend/src/features/chatting/project/Project.tsx
@@ -2,19 +2,26 @@ import { ChatPane } from "../chat/Chat";
 import type { ProjectViewModel } from "./useProject";
 
 function ProjectPane({
-    chats,
-    settings,
-    setSettings,
-    setChat,
-    findChatById
-    }: ProjectViewModel) {
+  chats,
+  settings,
+  setSettings,
+  setChat,
+  findChatById,
+}: ProjectViewModel) {
   return (
-    <div>
-      {
-        chats.map((t) =>
-          <ChatPane key={t.id} {...t} setChat={setChat} findChatById={findChatById} />
-        )
-      }
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: "1rem",
+        overflowX: "auto",
+      }}
+    >
+      {chats.map((t) => (
+        <div key={t.id} style={{ minWidth: "300px", flex: "1 1 0" }}>
+          <ChatPane {...t} setChat={setChat} findChatById={findChatById} />
+        </div>
+      ))}
       <div>
         {JSON.stringify(settings)}
         {JSON.stringify(setSettings)}
