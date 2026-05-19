0c3ce556a208b0284d1ef6a98b5624f79541704c
Marc Merten <merten@eecc.info>
2026-05-19 11:34:04 +0200

    - Small fix

--- frontend/src/features/chatting/project/project-context.ts
+++ frontend/src/features/chatting/project/project-context.ts
@@ -2,19 +2,23 @@ import { createContext } from "react";
 import type { ProjectViewModel } from "./project-types";
 
 const defaultProjectViewModel: ProjectViewModel = {
-    settings: {
-        llms: [],
-        modes: []
-    },
-    chats: [],
+  settings: {
+    llms: [],
+    modes: [],
+  },
+  chats: [],
   setSettings: () => {},
   setChat: () => {},
   findChatById: () => null,
   findModeById: () => null,
   findLLMById: () => null,
-  setChats: () => null,
-}
+  setChats: () => {},
+  defaultLLM: {
+    id: null,
+    useReasoning: false,
+  },
+};
 
-const ProjectContext = createContext<ProjectViewModel>(defaultProjectViewModel)
+const ProjectContext = createContext<ProjectViewModel>(defaultProjectViewModel);
 
-export default ProjectContext
+export default ProjectContext;
